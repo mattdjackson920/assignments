f77f5a48bc66c817f5a9a32612d4e4b969dbf070
mattdjackson920 <mattdjackson920@gmail.com>
2026-06-28 17:31:06 -0700

    Update first_program.asm

--- CISC_211/week2/first_program.asm
+++ CISC_211/week2/first_program.asm
@@ -1,1 +1,19 @@
+section .data
+    message db "I came,", 10, "I saw,", 10, "I conquered.", 10
+    message_len equ $ - message
 
+section .text
+    global _start
+
+_start:
+    ; write message to screen
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, message_len
+    syscall
+
+    ; exit program
+    mov rax, 60
+    mov rdi, 0
+    syscall
